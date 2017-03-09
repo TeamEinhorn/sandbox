43ef0fe8841626d0aa18fa38c6405110ed6c2a7f
Joachim J Schmidt <joachim-j.schmidt@deutschebahn.com>
2017-03-09 13:26:46 +0100

    Added vert.x web and health check

--- src/main/java/de/teameinhorn/sandbox/MainVerticle.java
+++ src/main/java/de/teameinhorn/sandbox/MainVerticle.java
@@ -1,12 +1,30 @@
 package de.teameinhorn.sandbox;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.healthchecks.HealthCheckHandler;
+import io.vertx.ext.healthchecks.Status;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
-    @Override
-    public void start() throws Exception {
-        vertx.createHttpServer().requestHandler(req -> req.response().end("Hello World!")).listen(8080);
-    }
+	@Override
+	public void start() throws Exception {
+
+		HealthCheckHandler hch = HealthCheckHandler.create(vertx).register("main", future -> {
+			future.complete(Status.OK());
+		});
+
+		Router router = Router.router(vertx);
+
+		router.get("/hello/:name")//
+				.handler(context -> {
+					String name = context.request().getParam("name");
+					context.response().end("Hello " + name + "!");
+				});
+		
+		router.get("/status").handler(hch);
+
+		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+	}
 
 }
